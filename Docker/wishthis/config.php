5a2bb2bb7f5306b5025f4a2f9eaa8456bc81269e
Sergei Kozlov <sergeo.kozlov@gmail.com>
2023-11-11 12:30:26 +0100

    withthis

---
+++ Docker/wishthis/config.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Configuration
+ *
+ * Usually, the wishthis installer will create the config.php for you.
+ */
+
+namespace wishthis;
+
+/**
+ * Database
+ *
+ * Used to connect to your MySQL database.
+ */
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', 'wishthis');
+define('DATABASE_USER', 'root');
+define('DATABASE_PASSWORD', 'root');
+
+/**
+ * Development
+ *
+ * Keep this disabled for production sites.
+ */
+define('ENV_IS_DEV', false);
+
+/**
+ * Channels
+ *
+ * It's safe to delete this if you are self-hosting. Alternatively you can
+ * replace these branches and domains with your own.
+ */
+define(
+    'CHANNELS',
+    array(
+        array(
+            'branch' => 'stable',
+            'host'   => 'wishthis.online',
+            'label'  => __('Stable'),
+        ),
+        array(
+            'branch' => 'release-candidate',
+            'host'   => 'rc.wishthis.online',
+            'label'  => __('Release candidate'),
+        ),
+    )
+);
+
+/**
+ * plausible
+ *
+ * Whether to make calls to plausible.io.
+ */
+define('PLAUSIBLE', false);
+
+/**
+ * Miscellaneous
+ */
+define('DISABLE_USER_REGISTRATION', false);
